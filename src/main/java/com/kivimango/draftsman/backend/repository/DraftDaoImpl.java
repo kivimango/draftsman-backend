55753f84e584bf84e205da72cb286ce907bbc4a0
kivimango <balazsviktor23@gmail.com>
2018-03-04 11:58:29 +0100

    Added findById(), reqrote queries

--- src/main/java/com/kivimango/draftsman/backend/repository/DraftDaoImpl.java
+++ src/main/java/com/kivimango/draftsman/backend/repository/DraftDaoImpl.java
@@ -2,6 +2,7 @@ package com.kivimango.draftsman.backend.repository;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.stereotype.Repository;
 import com.kivimango.draftsman.backend.domain.Draft;
@@ -15,18 +16,38 @@ import com.kivimango.draftsman.backend.domain.Draft;
 @Repository
 public class DraftDaoImpl implements DraftRepository {
 	
+	@Value("${image.repository.path}")
+	private static String fileRepoPath;
+	
+	private final DraftRowMapper rowMapper;
+	
 	private JdbcOperations jdbc;
-	private final DraftRowMapper rowMapper = new DraftRowMapper();
-
+	
 	@Autowired
-	public DraftDaoImpl(JdbcOperations jdbc) {
+	public DraftDaoImpl(JdbcOperations jdbc, @Value("${image.repository.path}") String path) {
 		this.jdbc = jdbc;
+		fileRepoPath = path;
+		rowMapper = new DraftRowMapper(fileRepoPath);
 	}
 
 	@Override
 	public List<Draft> findByPartNumber(String pn) {
 		String like = "%" + pn + "%";
-		return jdbc.query("SELECT * FROM DR_DRAFTS WHERE part_number LIKE ? OR part_number = ?", new Object[]{like, pn}, rowMapper);
+		return jdbc.query("SELECT dr_id, img_src, part_number, version, sheet FROM DR_DRAFTS WHERE part_number LIKE ? OR part_number = ?;", 
+			(rs, i) -> 
+				new Draft.Builder().id(rs.getInt(1))
+				.image(fileRepoPath + rs.getString(2))
+				.partNumber(rs.getString(3))
+				.version(rs.getString(4))
+				.sheet(rs.getInt(5))
+				.build(), 
+		like, like);
+	}
+
+	@Override
+	public Draft findByID(Integer id) {
+		return jdbc.query("SELECT * FROM DR_DRAFTS WHERE dr_id = ? LIMIT 1;", 
+		rs -> rs.next() ? rowMapper.mapRow(rs, 1) : null, id);
 	}
 
 }
--- src/main/java/com/kivimango/draftsman/backend/repository/DraftRepository.java
+++ src/main/java/com/kivimango/draftsman/backend/repository/DraftRepository.java
@@ -11,6 +11,22 @@ import com.kivimango.draftsman.backend.domain.Draft;
 
 public interface DraftRepository {
 	
+	/** 
+	 * Searches the database for the given part number.It lists similar part numbers to the query, 
+	 * or/and the exact part number given.
+	 * @param partNumber
+	 * @return A list containing the matching items, or an empty list if nothing found.
+	*/
+	
 	List<Draft> findByPartNumber(String pn);
+
+	/**
+	 * Searches an item in the database for the given id.
+	 * The search should find only one row on matching.
+	 * @param id the unique id of the item
+	 * @return the mapped row as Draft object, or null if no result
+	 */
+	
+	Draft findByID(Integer id);
 
 }
