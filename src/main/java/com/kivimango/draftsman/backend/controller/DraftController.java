73e93324f99f5be96342282eb2399c7f6759c37b
kivimango <balazsviktor23@gmail.com>
2018-03-04 13:52:21 +0100

    Added viewDraft()

--- src/main/java/com/kivimango/draftsman/backend/controller/DraftController.java
+++ src/main/java/com/kivimango/draftsman/backend/controller/DraftController.java
@@ -1,7 +1,10 @@
 package com.kivimango.draftsman.backend.controller;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.kivimango.draftsman.backend.domain.DraftDTO;
+import com.kivimango.draftsman.backend.exception.DraftNotFoundException;
 import com.kivimango.draftsman.backend.service.DraftService;
 
 /**
- * @author sapka
+ * @author kivimango
  * @since 0.1
  * @version 0.1
  */
@@ -30,17 +34,48 @@ class DraftController {
 	/**
 	 * Searches the database for the given part number.It lists similar part numbers to the query, 
 	 * or/and the exact part number given.
-	 * @param partNumber The part number the user is looking for
+	 * @param partNumber The draft the user is looking for
 	 * @return The result of the search with code 200, or an empty list if nothing found with code 204.
 	 */
 	
-	@GetMapping(value="/drafts/{partNumber}", produces={ MediaType.APPLICATION_JSON_VALUE} )
+	@GetMapping(value="/drafts/list/{partNumber}", produces={ MediaType.APPLICATION_JSON_VALUE} )
 	ResponseEntity<List<DraftDTO>> listDrafts(@PathVariable(value="partNumber", required=true) final String partNumber) {
+		if(partNumber == null || partNumber.isEmpty() || partNumber.length() < 3) throw new IllegalArgumentException("Parameter partNumber length must be at least 3 character long");
 		final List<DraftDTO> results = drafts.findByPartNumber(partNumber);
 		if(results.isEmpty()) {
 			return new ResponseEntity<List<DraftDTO>>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<List<DraftDTO>>(results, HttpStatus.OK);
+	}
+	
+	/**
+	 * Searches the database for a draft by the given id.
+	 * NOTE: clients should accept both media types image/jpeg and application/json
+	 * TODO: if "produces" info given in the annotation, spring will throw a HttpMediaTypeNotAcceptableException
+	 * @param id The (datastore) id of the draft
+	 * @return The image of the draft included in the response,
+	 * or a json response with errorMessage property describing the error
+	 * @throws DraftNotFoundException
+	 * @throws IOException
+	 */
+	
+	@GetMapping(value="/drafts/view/{id}")
+	ResponseEntity<byte[]> viewDraft(@PathVariable(value="id", required=true) final Integer id) 
+			throws DraftNotFoundException, IOException {
+		final DraftDTO draft = drafts.findById(id);
+		byte[] content = readPicture(draft);
+		final HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.IMAGE_JPEG);
+	    headers.setContentLength(content.length);
+		return new ResponseEntity<byte[]>(content, headers, HttpStatus.OK);
+	}
+	
+	private byte[] readPicture(DraftDTO draft) throws IOException {
+		final RandomAccessFile file = new RandomAccessFile(draft.getImage(), "r");
+		byte[] content = new byte[(int) file.length()];
+		file.readFully(content);
+		file.close();
+		return content;
 	}
 
 }
--- src/test/java/com/kivimango/draftsman/backend/controller/DraftControllerTest.java
+++ src/test/java/com/kivimango/draftsman/backend/controller/DraftControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.verify;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,8 +13,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -24,6 +27,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.kivimango.draftsman.backend.domain.DraftDTO;
 import com.kivimango.draftsman.backend.domain.DraftSamples;
+import com.kivimango.draftsman.backend.exception.DraftNotFoundException;
 import com.kivimango.draftsman.backend.service.DraftService;
 
 /**
@@ -39,45 +43,92 @@ public class DraftControllerTest extends DraftSamples {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Value("${image.repository.path}")
+	private String testRepoPath;
+	
+	@Value("classpath:sample.jpg")
+	Resource drawing;
+	
 	@MockBean
 	private DraftService service;
 	
 	private String partNumber = "123-4567";
-	private String URI = "/drafts/";
-	private List<DraftDTO> list = Arrays.asList(new DraftDTO(id, partNumber, version, sheet));
+	private List<DraftDTO> list = Arrays.asList(new DraftDTO(id, image, partNumber, version, sheet));
 	
 	@Test
 	public void testFindByPartNumber() throws Exception {
-		Mockito.when(service.findByPartNumber(partNumber)).thenReturn(list);
-		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI + partNumber).accept(MediaType.APPLICATION_JSON)
-				.contentType(MediaType.APPLICATION_JSON);
-		
-		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-		MockHttpServletResponse response = result.getResponse();
-		String responseStr = response.getContentAsString();
+		String URI = "/drafts/list/";
+		MockHttpServletResponse response = buildRequestAndPerform(URI + partNumber); 
 	
 		assertEquals(HttpStatus.OK.value(), response.getStatus());
 		assertEquals(MediaType.APPLICATION_JSON_UTF8_VALUE, response.getContentType());
-		assertThat(responseStr, containsString(partNumber));
+		assertThat(response.getContentAsString(), containsString(partNumber));
 		verify(service).findByPartNumber(partNumber);
 	}
 	
 	@Test
-	public void testFindByPartNumberWithNoneExistingPartNumber() throws Exception {
+	public void testFindByPartNumberWithNoneExistingPartNumberShouldReturnEmptyListAndCode204() throws Exception {
+		String URI = "/drafts/list/";
 		String nonExistentPartNumber = "000-0000";
 		List<DraftDTO> emptyList = new ArrayList<DraftDTO>(0);
 		Mockito.when(service.findByPartNumber(partNumber)).thenReturn(emptyList);
 		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI+ nonExistentPartNumber).accept(MediaType.APPLICATION_JSON)
-				.contentType(MediaType.APPLICATION_JSON);
-		
-		MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
+		MockHttpServletResponse response = buildRequestAndPerform(URI + nonExistentPartNumber);
 		String responseStr = response.getContentAsString();
 	
 		assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
 		assertTrue(responseStr.isEmpty());
 		verify(service).findByPartNumber(nonExistentPartNumber);
+	}
+	
+	@Test
+	public void testFindByPartNumberWithParameterLessThanMinimal() throws Exception {
+		String URI = "/drafts/list/12";	
+		RequestBuilder rb = MockMvcRequestBuilders.get(URI).accept(MediaType.APPLICATION_JSON_UTF8_VALUE);
+		MockHttpServletResponse response = mockMvc.perform(rb).andReturn().getResponse();
+		
+		assertEquals("{\"errorMessage\":\"Parameter partNumber length must be at least 3 character long\"}", response.getContentAsString());
+	}
+	
+	@Test
+	public void testViewDraftShouldReturnImageAndCode200() throws Exception {
+		String URI = "/drafts/view/";
+		DraftDTO draft = new DraftDTO(id, drawing.getFile().getAbsolutePath(), partNumber, version, sheet);
+		Mockito.when(service.findById(id)).thenReturn(draft);
+		
+		RequestBuilder rb = MockMvcRequestBuilders.get(URI + id.toString()).accept(MediaType.IMAGE_JPEG_VALUE);
+		MockHttpServletResponse response = mockMvc.perform(rb).andReturn().getResponse();
+		byte[] body = response.getContentAsByteArray();
+		
+		// Reading the content of the sample picture
+		RandomAccessFile file = new RandomAccessFile(drawing.getFile(), "r");
+		byte[] actual = new byte[(int) file.length()];
+		file.readFully(actual);
+		file.close();
+		
+		assertTrue(Arrays.equals(actual, body));
+	}
+	
+	@Test
+	public void testViewDraftWithNonExistentParamShouldReturn404() throws Exception {
+		String URI = "/drafts/view/";
+		Mockito.when(service.findById(5)).thenThrow(new DraftNotFoundException(5));
+		
+		RequestBuilder rb = MockMvcRequestBuilders.get(URI + 5).accept(MediaType.IMAGE_JPEG_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE);
+		MockHttpServletResponse response = mockMvc.perform(rb).andReturn().getResponse();
+		
+		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+		assertEquals(MediaType.APPLICATION_JSON_UTF8_VALUE, response.getContentType());
+		assertEquals("{\"errorMessage\":\"The requested item (5) could nout be found !\"}", response.getContentAsString());
+	}
+	
+	private MockHttpServletResponse buildRequestAndPerform(String uri) throws Exception {
+		Mockito.when(service.findByPartNumber(partNumber)).thenReturn(list);
+		RequestBuilder rb = MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON);
+		MvcResult result = mockMvc.perform(rb).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		return response;
 	}
 
 }
